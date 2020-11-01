ba03f4b4c8569b37c7f600750d4cf565ec8e2fed
baselqyqony <baselqyqony@gmail.com>
2020-11-01 19:41:13 +0200

    add MeasurementProfile class and add it as member to packet class

---
+++ include/pipert/MeasurementProfile.h
@@ -0,0 +1,28 @@
+#ifndef _CHANNELIMPL_H_
+#define _CHANNELIMPL_H_
+
+#include <utility>
+
+#include "pipert/PacketBase.h"
+#include "pipert/Timer.h"
+namespace pipert {
+
+template <class T>
+class MeasurementProfile {
+
+	public:
+  const char* MesurementProfileName;
+  const char* ChannelName; 
+  Timer::Time packetTimeStamp;
+  Timer::Time AcquireTime;
+  Timer::Time PushTime;
+  Timer::Time PopTime;
+  Timer::Time ExecutionStartTime;
+  Timer::Time DroppingTime;
+  std::vector<char*> attributesToBeRecorded; 
+
+
+
+};
+}  // namespace pipert
+#endif
