3137dcef4a2ab4fcd0efa29fbf94e770ce7944f3
baselqyqony <baselqyqony@gmail.com>
2020-11-03 08:51:08 +0200

    add UDPConnection manager prototype class which will manage UDP connection

---
+++ include/pipert/UDPConnection.h
@@ -0,0 +1,16 @@
+#ifndef _CHANNELIMPL_H_
+#define _CHANNELIMPL_H_
+
+#include <utility>
+
+#include "pipert/PacketBase.h"
+#include "pipert/Timer.h"
+namespace pipert {
+
+template <class T>
+class UDPConnection {
+ private:
+  // this class will manage udp connection  
+
+}  // namespace pipert
+#endif #pragma once
