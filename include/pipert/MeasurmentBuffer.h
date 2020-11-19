9a03dea3ac2ffa63407cf306f2128ea094aa86f6
baselqyqony <baselqyqony@gmail.com>
2020-11-19 08:23:37 +0200

    add pushMeasurement function ,sendingJob Thread and sendingJobCallback function

--- include/pipert/MeasurmentBuffer.h
+++ include/pipert/MeasurmentBuffer.h
@@ -7,18 +7,66 @@
 #include "pipert/Timer.h"
 namespace pipert {
 
-template <class T>
+
 class MeasurementBuffer {
 
-public:
-
-  void pushMeasurement(MeasurementProfileBase profile);
-   
-
 private:
-  PushBufferToUDPConnection();
+  // this function will start sending the buffer via UDP connection each
+  // timeInterval and clear measuremensListList
+  void SendingJobCallBack() { 
+	  
+	  while (enableSending) {
+
+		    if (measuremensListList.size() == BufferSize) {
+              connection.send(measuremensListList);
+                      measuremensListList.clear();
+            }
+            std::this_thread::sleep_for(std::chrono::milliseconds(timeInterval));
+
+    }
+  
+  }
+
+  
+ 
+ std::thread SendingJob(SendingJobCallBack);
+
+
+ public:
+ uint16_t BufferSize;
+ uint16_t TimeInterval;
+ bool enableSending;
  UDPConnection connection;
- int BufferSize;
+
+ std::vector<MeasurementProfileBase> measuremensListList;
+
+
+ //recieve function
+
+ MeasurementBuffer(UDPConnection udpConnection, uint16_t timeIntervals,
+                   uint16_t bufferSize;) {
+   connection = udpConnection;
+   BufferSize = bufferSize;
+   TimeIntervals = timeIntervals;
+   SendingJob.join();
+
+
+
+ }
+
+
+
+ void pushMeasurement(MeasurementProfileBase profile) 
+ {
+   if (measuremensListList.size() < BufferSize) {
+     measuremensListList.add(profile);
+   }
+ 
+ }
+
+ 
+
+
 
 }  // namespace pipert
 #endif #pragma once
