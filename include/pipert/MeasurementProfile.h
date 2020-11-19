07fb01177d2b893b1ff96fbf52d6697a55e1b469
baselqyqony <baselqyqony@gmail.com>
2020-11-19 08:14:06 +0200

    add MeasurementProfileProcessStatus to MeasurementProfile

--- include/pipert/MeasurementProfile.h
+++ include/pipert/MeasurementProfile.h
@@ -1,15 +1,18 @@
-#ifndef _CHANNELIMPL_H_
+  #ifndef _CHANNELIMPL_H_
 #define _CHANNELIMPL_H_
 
 #include <utility>
 
 #include "pipert/PacketBase.h"
 #include "pipert/Timer.h"
+#include "pipert/MeasurementProfileBase.h"
 namespace pipert {
 
+
+//this enum define the last level of processing the packet reached in the channel
+enum MeasurementProfileProcessStatus { NoStatus,Acquired, Pushed, Popped,Executed,Dropped };
 template <class T>
-class MeasurementProfile : MeasurementProfileBase<class T> {
- public:
+class MeasurementProfile : MeasurementProfileBase<class T> { 
   const char* MesurementProfileName;
   const char* ChannelName;
   Timer::Time packetTimeStamp;
@@ -18,13 +21,17 @@ class MeasurementProfile : MeasurementProfileBase<class T> {
   Timer::Time PopTime;
   Timer::Time ExecutionStartTime;
   Timer::Time DroppingTime;
+  MeasurementProfileProcessStatus processStatus;
+
   int* threadID;
 
-  //must 
+	public:
+  MeasurementProfile() : MeasurementProfileBase(false) {
+    processStatus = MeasurementProfileProcessStatus.NoStatus;
+  }
 
-  
-	 public
-  MeasurementProfile() : MeasurementProfileBase(false) {}
+
+   
 };
 }  // namespace pipert
 #endif
